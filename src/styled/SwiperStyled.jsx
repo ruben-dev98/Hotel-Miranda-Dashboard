8c27bbc8ccb09bbb20b0d0b13e7a1daf487c112b
ruben.dopico.dev <ruben.dopico.dev@gmail.com>
2024-03-14 22:37:34 +0100

    FIX: styles swipers button

--- src/styled/SwiperStyled.jsx
+++ src/styled/SwiperStyled.jsx
@@ -17,6 +17,29 @@ width: 100%;
 }
 
     .swiper-button-next, .swiper-button-prev {
-        
+        border: 1px #FFF solid;
+        background-color: #FFFFFF47;
+        width: 30px;
+        height: 30px;
+        border-radius: 10px;
+        bottom: 150px;
+        top: auto;
+
+        &::after {
+            font-size: 1.2rem;
+            color: #FFF;
+        }
+    }
+    
+    .swiper-button-prev {
+        left: 10%;
+    }
+
+    .swiper-button-next {
+        right: 10%;
+    }
+
+    .swiper-button-disabled {
+        background-color: #FFFFFF1A;
     }
 `
