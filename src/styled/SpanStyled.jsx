e3b9b85cba634ec9d68067c17934ee2fcee48a41
ruben.dopico.dev <ruben.dopico.dev@gmail.com>
2024-03-15 12:11:13 +0100

    FIX: const on other file

--- src/styled/SpanStyled.jsx
+++ src/styled/SpanStyled.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import { styled } from "styled-components";
 
 export const SpanStyled = styled.span`
     display: flex;
--- src/styled/SwiperStyled.jsx
+++ src/styled/SwiperStyled.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import { styled } from "styled-components";
 import { Swiper } from 'swiper/react';
 
 export const SwiperStyled = styled(Swiper)`
